82af356898d4ccd3fae5a29115c30a4e3e45dc1f
sorim492 <sorim492@gmail.com>
2024-01-03 00:26:21 -0600

    app created

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <img src={logo} className="img-fluid app-logo" alt="logo" />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
