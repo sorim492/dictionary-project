87e986105926402d15750a34e49c4e168c121555
sorim492 <sorim492@gmail.com>
2024-01-07 13:49:18 -0600

    update meaning, results and definition

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,14 @@
 import React, {useState} from "react";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary(){
     let [keyword, setKeyword] = useState("");
+    let [results, setResults] = useState(null);
 
     function handleResponse(response){
-        console.log(response.data);
+        console.log(response.data[0].meanings[0].definitions[0].definition)
+        setResults(response.data[0])
     }
 
     //https://dictionaryapi.dev/ Documentation
@@ -25,6 +28,8 @@ export default function Dictionary(){
                 <input type="search" onChange={handleKeyword}/>
                 <input type="submit" value="Search"/>
             </form>
+            <Results results = {results}/>
+        
         </div>
     )
 }
---
+++ src/Results.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Meaning from "./Meaning";
+
+export default function Results(props){
+
+if (props.results){
+    return(
+        <div className="results">
+            <h2>{props.results.word}</h2>
+            <h3>{props.results.meanings.map(function(meaning, index){return (
+                <div key={index}>
+                    <Meaning meaning={meaning}/>
+                </div>
+            )})}</h3>
+        </div>
+    )
+    } else { return null }
+}
